7834567402bcdfd797d36499eff2d1380017ff02
aurielcp <auriel.castroperez@gmail.com>
2024-11-24 22:21:31 -0600

    Página login

---
+++ index.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login de Sistema</title>
+    <link rel="stylesheet" href="estilos/stylesIndex.css"> 
+    <script src="estilos/scriptsIndex.js" defer></script> 
+
+
+</head>
+    
+<header>
+        <nav class="nav-menu">
+            <ul>
+                <li><a href="index.php">Inicio</a></li>
+            </ul>
+        </nav>
+</header>
+
+<body>
+    <div class="row">
+        <div class="col s12 m5">
+            <div>
+                <h1>Login de sistema</h1>
+                <form method="POST" action="logica/loguear.php">
+                    <input type="text" name="no_cuenta" placeholder="Número de Cuenta" required />
+                    <input type="text" name="clave" placeholder="Contraseña" required />
+                    <button type="submit">Iniciar Sesión</button>
+                </form>
+            </div>
+        </div>
+    </div>
+
+<footer>
+<p>&copy; 2024 Sistema de Gestión de Alumnos. Todos los derechos reservados.</p>
+    <div class="social-links">
+        <a href="#">Facebook</a>
+        <a href="#">Telegram</a>
+        <a href="#">Github</a>
+    </div>
+</footer>
+
+</body>
+</html>
